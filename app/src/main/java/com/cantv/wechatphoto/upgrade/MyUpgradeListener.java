8a21cc74c654712f6a9c76900917ce96c0da1108
kylinhaha <kylinhaha@gmail.com>
2016-12-16 11:10:36 +0800

    去除bugly多於方法

--- app/src/main/java/com/cantv/wechatphoto/upgrade/MyUpgradeListener.java
+++ app/src/main/java/com/cantv/wechatphoto/upgrade/MyUpgradeListener.java
@@ -3,13 +3,8 @@ package com.cantv.wechatphoto.upgrade;
 import com.cantv.wechatphoto.App;
 import com.tencent.bugly.beta.UpgradeInfo;
 import com.tencent.bugly.beta.upgrade.UpgradeListener;
-import com.tencent.bugly.proguard.aa;
 
 public class MyUpgradeListener implements UpgradeListener {
-    @Override
-    public boolean onUpgrade(aa aa, int i, String s) {
-        return false;
-    }
 
     @Override
     public void onUpgrade(int ret, UpgradeInfo strategy, boolean isManual, boolean
