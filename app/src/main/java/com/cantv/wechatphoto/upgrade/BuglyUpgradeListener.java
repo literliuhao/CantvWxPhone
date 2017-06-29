2987e42ca898c8cce49b67fc944be9b5d41bafeb
fuwen <fuwen@cantv.cn>
2017-06-29 18:52:28 +0800

    添加升级SDK

--- app/src/main/java/com/cantv/wechatphoto/upgrade/BuglyUpgradeListener.java
+++ app/src/main/java/com/cantv/wechatphoto/upgrade/BuglyUpgradeListener.java
@@ -4,7 +4,7 @@ import com.cantv.wechatphoto.SampleApplicationLike;
 import com.tencent.bugly.beta.UpgradeInfo;
 import com.tencent.bugly.beta.upgrade.UpgradeListener;
 
-public class MyUpgradeListener implements UpgradeListener {
+public class BuglyUpgradeListener implements UpgradeListener {
 
     @Override
     public void onUpgrade(int ret, UpgradeInfo strategy, boolean isManual, boolean
